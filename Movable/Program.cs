b2c67158d9092cbcc7d1641a0e384b527a0c065a
Robert Clottey <rcnkclottey001@st.ug.edu.gh>
2024-06-17 23:39:29 +0000

    moving vehicles

---
+++ Movable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+
+    public interface IMovable
+    {
+        public void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is Moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is Moving");
+        }
+    }
+}
