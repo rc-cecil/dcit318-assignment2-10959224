cb02b1dd0462c90a4b60a0b64fe43d70a95e7030
Robert Clottey <rcnkclottey001@st.ug.edu.gh>
2024-06-17 23:35:45 +0000

    animal

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic Sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+}
